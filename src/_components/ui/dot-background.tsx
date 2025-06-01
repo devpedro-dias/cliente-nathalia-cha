9f1ae0bbe37048552f4d5ea7fb4d96e27894e60d
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-31 22:48:36 -0300

    feat: start here section done

--- src/_components/ui/dot-background.tsx
+++ src/_components/ui/dot-background.tsx
@@ -3,22 +3,23 @@ import { Highlight } from './hero-highlight'
 
 export const DotBackground = () => {
     return (
-        <div className="relative flex h-[50rem] w-full items-center justify-center bg-background z-50">
+        <div className="relative flex h-[50rem] w-full items-center justify-center bg-background z-0">
+            {/* ⬇️ Dots background */}
             <div
                 className={cn(
-                    'absolute inset-0',
+                    'absolute inset-0 z-0',
                     '[background-size:20px_20px]',
-                    '[background-image:radial-gradient(#e6e6e6_1px,transparent_1px)]',
+                    '[background-image:radial-gradient(#66ccff_1px,transparent_2px)]',
                     'dark:[background-image:radial-gradient(#0066FF_1px,transparent_2px)] opacity-50'
                 )}
             />
-            {/* Radial gradient for the container to give a faded look */}
-            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)] "></div>
-            {/* <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-                Backgrounds
-            </p> */}
-            <div className="max-w-[80vw] lg:max-w-[60vw] z-50">
-                <h1 className="font-semibold text-2xl sm:text-3xl md:text-4xl leading-snug text-center">
+
+            {/* ⬇️ Radial mask to fade center (no bg color, just a mask) */}
+            <div className="pointer-events-none absolute inset-0 z-10 [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)] [mask-repeat:no-repeat] [mask-size:100%]" />
+
+            {/* ⬇️ Content */}
+            <div className="max-w-[80vw] lg:max-w-[60vw] z-20 text-center">
+                <h1 className="font-semibold text-2xl sm:text-3xl md:text-4xl leading-snug">
                     Descubra o{' '}
                     <Highlight className="text-white">Chá Secreto</Highlight>{' '}
                     que está transformando o corpo de quem quer{' '}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
+import { Button } from '@/_components/ui/button'
 import { DotBackground } from '@/_components/ui/dot-background'
+import { Highlight } from '@/_components/ui/hero-highlight'
+import { Coffee, CornerRightDown } from 'lucide-react'
+import Link from 'next/link'
 
 const HomePage = () => {
     return (
-        <main>
+        <main className="flex flex-col gap-10">
             <section>
                 <DotBackground />
+            </section>
+            <section className="flex flex-col gap-6 items-center justify-center text-center max-w-[80vw] lg:max-w-[60vw] mx-auto">
+                <h2 className="flex justify-center items-center font-semibold text-2xl sm:text-3xl md:text-4xl leading-snug text-center gap-2">
+                    <div>
+                        <Highlight className="text-white">Comece</Highlight> por
+                        aqui
+                    </div>
+                    <CornerRightDown />
+                </h2>
+
+                <p className="font-medium text-md text-zinc-600">
+                    Entenda mais sobre como funciona essa receita, e o que te
+                    escondem sobre o emagrecimento rápido.
+                </p>
+
+                <div className="h-[600px] w-[300px] bg-zinc-200 flex items-center justify-center">
+                    vídeo
+                </div>
+
+                <Button size="lg" asChild>
+                    <Link href="#">
+                        <Coffee />
+                        Comece agora
+                    </Link>
+                </Button>
+            </section>
+
+            <section className="max-w-[80vw] lg:max-w-[60vw] mx-auto">
+                <h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl text-center">
+                    Para quem serve?
+                </h2>
             </section>
         </main>
     )
