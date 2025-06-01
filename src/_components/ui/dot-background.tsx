35be22e8cafb4a27bede46dfa00e0cdd7016288c
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-31 23:57:54 -0300

    feat: add animations on content enter page

--- src/_components/ui/dot-background.tsx
+++ src/_components/ui/dot-background.tsx
@@ -1,10 +1,10 @@
 import { cn } from '@/_lib/utils'
 import { Highlight } from './hero-highlight'
+import { CSSProperties } from 'react'
 
 export const DotBackground = () => {
     return (
         <div className="relative flex h-[50rem] w-full items-center justify-center bg-background z-0">
-            {/* ⬇️ Dots background */}
             <div
                 className={cn(
                     'absolute inset-0 z-0',
@@ -14,12 +14,13 @@ export const DotBackground = () => {
                 )}
             />
 
-            {/* ⬇️ Radial mask to fade center (no bg color, just a mask) */}
             <div className="pointer-events-none absolute inset-0 z-10 [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)] [mask-repeat:no-repeat] [mask-size:100%]" />
 
-            {/* ⬇️ Content */}
             <div className="max-w-[80vw] lg:max-w-[60vw] z-20 text-center">
-                <h1 className="font-semibold text-2xl sm:text-3xl md:text-4xl leading-snug">
+                <h1
+                    className="font-semibold text-2xl sm:text-3xl md:text-4xl leading-snug fade-in-up"
+                    style={{ '--delay': '.5s' } as CSSProperties}
+                >
                     Descubra o{' '}
                     <Highlight className="text-white">Chá Secreto</Highlight>{' '}
                     que está transformando o corpo de quem quer{' '}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { TESTIMONIALS_CARDS } from '@/_data'
 import { CoffeeIcon, CornerRightDown, MessageCircle } from 'lucide-react'
 
 import Link from 'next/link'
+import { CSSProperties } from 'react'
 
 const HomePage = () => {
     return (
@@ -14,8 +15,12 @@ const HomePage = () => {
             <section>
                 <DotBackground />
             </section>
+
             <section className="flex flex-col gap-6 items-center justify-center text-center max-w-[80vw] lg:max-w-[60vw] mx-auto">
-                <h2 className="flex justify-center items-center font-semibold text-2xl sm:text-3xl md:text-4xl leading-snug text-center gap-2">
+                <h2
+                    className="flex justify-center items-center font-semibold text-2xl sm:text-3xl md:text-4xl leading-snug text-center gap-2 fade-in-up"
+                    style={{ '--delay': '.8s' } as CSSProperties}
+                >
                     <div>
                         <Highlight className="text-white">Comece</Highlight> por
                         aqui
@@ -23,7 +28,10 @@ const HomePage = () => {
                     <CornerRightDown />
                 </h2>
 
-                <p className="font-medium text-md text-zinc-600">
+                <p
+                    className="font-medium text-md text-zinc-600 fade-in-up"
+                    style={{ '--delay': '1s' } as CSSProperties}
+                >
                     Entenda mais sobre como funciona essa receita, e o que te
                     escondem sobre o emagrecimento rápido.
                 </p>
@@ -41,8 +49,11 @@ const HomePage = () => {
             </section>
 
             <section className="flex flex-col gap-10 bg-gradient-to-r from-blue-400 to-indigo-600 py-10">
-                <div className=" text-white ">
-                    <h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl text-center">
+                <div className="text-white">
+                    <h2
+                        className="font-semibold text-2xl sm:text-3xl md:text-4xl text-center fade-in-up"
+                        style={{ '--delay': '.8s' } as CSSProperties}
+                    >
                         Para quem serve?
                     </h2>
                 </div>
@@ -59,11 +70,17 @@ const HomePage = () => {
             </section>
 
             <section className="flex flex-col gap-6 items-center justify-center text-center">
-                <h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl text-center">
+                <h2
+                    className="font-semibold text-2xl sm:text-3xl md:text-4xl text-center fade-in-up"
+                    style={{ '--delay': '.8s' } as CSSProperties}
+                >
                     Depoimentos
                 </h2>
 
-                <p className="text-md text-zinc-600">
+                <p
+                    className="text-md text-zinc-600 fade-in-up"
+                    style={{ '--delay': '1s' } as CSSProperties}
+                >
                     Alguns dos depoimentos dos nossos alunos que se juntaram a
                     nossa comunidade
                 </p>
@@ -90,11 +107,17 @@ const HomePage = () => {
             </section>
 
             <section className="flex flex-col gap-6 items-center justify-center text-center py-10">
-                <h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl text-center">
+                <h2
+                    className="font-semibold text-2xl sm:text-3xl md:text-4xl text-center fade-in-up"
+                    style={{ '--delay': '.8s' } as CSSProperties}
+                >
                     Alguma dúvida?
                 </h2>
 
-                <p className="text-base text-zinc-600">
+                <p
+                    className="text-base text-zinc-600 fade-in-up"
+                    style={{ '--delay': '1s' } as CSSProperties}
+                >
                     Entre em contato conosco por nosso canal de suporte,
                     responderemos em até 48 horas.
                 </p>
