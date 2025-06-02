8a9825191f36b93604977d92a98fe38ce2b1dbee
Pedro Dias <pedrocorchogdev@gmail.com>
2025-06-02 18:54:47 -0300

    fix: display grid cols in 1440px width view port

--- src/_components/cards-section.tsx
+++ src/_components/cards-section.tsx
@@ -9,6 +9,7 @@ import {
     RefreshCw,
     TrendingUp,
 } from 'lucide-react'
+import '@/styles/breakpoints.css'
 
 const ICONS = {
     CalendarHeart: CalendarHeart,
@@ -21,13 +22,13 @@ const ICONS = {
 
 const CardsSection = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 xxl:grid-cols-3 justify-center gap-6">
             {CARDS_CONTENTS.map(({ id, icon, title, description }) => {
                 const LucideIcon = ICONS[icon as keyof typeof ICONS]
 
                 return (
                     <CardContainer key={id}>
-                        <CardBody className="w-[300px] h-[300px] bg-transparent rounded-xl shadow-xl p-6 border border-zinc-200 ">
+                        <CardBody className="w-full max-w-[300px] h-[300px] bg-transparent rounded-xl shadow-xl p-6 border border-zinc-200">
                             <CardItem
                                 translateZ={30}
                                 className="flex flex-col items-start gap-4 h-full"
@@ -35,10 +36,10 @@ const CardsSection = () => {
                                 <div className="text-white dark:text-blue-400">
                                     <LucideIcon size={32} />
                                 </div>
-                                <h3 className="text-xl font-semibold text-white">
+                                <h3 className="text-lg sm:text-xl font-semibold text-white">
                                     {title}
                                 </h3>
-                                <p className="text-sm text-zinc-200">
+                                <p className="text-xs sm:text-sm text-zinc-200">
                                     {description}
                                 </p>
                             </CardItem>
